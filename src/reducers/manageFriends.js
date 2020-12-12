f40885332728bcb6c8f2cfe0db2b718f13403d3e
Jon Noriega <Noriega331@gmail.com>
2020-12-12 17:05:43 -0500

    commit tree c0d2cb9e11f544cf073fa69cc994ea35deddc7dd

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,22 @@
+
+const state = {
+    friends: []
+}
+
+const action = {
+    type: "ADD_FRIEND",
+    friend: {
+        name: "Chrome Boi",
+        hometown: "NYC",
+        id: 1
+    }
+}
+
 export function manageFriends(state, action){
+    switch (action.type){
+        case "ADD_FRIEND":
+            return {friend: state.friend}
+        default:
+            return state
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,18 @@
-export function managePresents(state, action){
+
+const state = {
+    numberOfPresents: 0
 }
+
+const action = {
+    type: "INCREASE"
+}
+
+export function managePresents(state, action){
+    switch (action.type) {
+        case "INCREASE":
+            return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return state
+    }
+}
+
